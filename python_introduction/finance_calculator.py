bd53cb6b1e389e8dc406adc51b494bd58d1ac9a9
hum1235 <4hacking86@gmail.com>
2025-05-23 23:14:40 +0300

    saving fix

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses:"))
-savings = income - expenses
-projected_savings = savings * 12 + (savings * 12 * 0.05)
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses:"))
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print("Your monthly savings are $", savings)
+print("Your monthly savings are $", monthly_savings)
 print("Projected savings after one year, with interest, is: $", projected_savings)
