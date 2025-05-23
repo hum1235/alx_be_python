815d9fc36e42c13abb45ddaa7d3052e47d045e33
hum1235 <4hacking86@gmail.com>
2025-05-23 22:35:36 +0300

    python introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 5
+number2 = 10
+add = number1 + number2
+mult = number1 * number2
+sub = number2 - number 1
+
+print ("Addition of ", number1 "and", number2 ,"is",add)
+print ("Subtraction  of ", number2 "and", number1 ,"is",sub)
+print ("Multiplication  of ", number1 "and", number2 ,"is",mult)
