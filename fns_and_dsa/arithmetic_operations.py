e0e7016d7003c35f2d93d47eefc7e0e1f6d41481
hum1235 <4hacking86@gmail.com>
2025-06-08 22:17:56 +0300

    second fix

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,8 +8,9 @@ def perform_operation(num1, num2, operation):
         case "multiply":
             return num1 * num2
         case "divide":
-            if (num2 == 0):
-                print("Number can't be divided by zero")
-            else:
+            if (num2 != 0):
                 return num1/num2
+            elif (num2 == 0):
+                print("Number can't be divided by zero")
+                
 
