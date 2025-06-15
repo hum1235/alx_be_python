c42f1b56e91879b377db6c2ac40c38659c8152c0
hum1235 <4hacking86@gmail.com>
2025-06-15 21:40:22 +0300

    classess in oop

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,11 @@
+class BankAccount:
+    def _init_(account_balance):
+        self.account_balance = 0
+    def deposit(amount):
+        account_balance += amount
+        return account_balance
+    def withdraw(amount):
+        account_balance -= amount
+        return account_balance
+    def display_balance():
+        return account_balance
